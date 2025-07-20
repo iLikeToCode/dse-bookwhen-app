2d5a3f6a849c8a75322bb3700f4c660934657769
iLikeToCode <archie@discoverystem.org.uk>
2025-07-20 16:40:48 +0100

    Initial Upload

---
+++ app/src/main/java/com/archiehurst/bookwhen/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.archiehurst.bookwhen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient() // ensures it stays in-app
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://melissabutt.bookwhen.com/schedules")
+    }
+}
